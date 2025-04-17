3754dffc8e82a190e22067f105e4820bac3fdfae
Himanshuu05 <himanshupaswan987@gmail.com>
2025-04-18 02:01:53 +0530

    Add files via upload

---
+++ LongestCommonPrefix.java
@@ -0,0 +1,23 @@
+public class LongestCommonPrefix {
+    public static String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0) return "";
+
+        for (int i = 0; i < strs[0].length(); i++) {
+            char c = strs[0].charAt(i);
+
+            for (int j = 1; j < strs.length; j++) {
+                // If index i is out of bounds or characters mismatch
+                if (i >= strs[j].length() || strs[j].charAt(i) != c) {
+                    return strs[0].substring(0, i);
+                }
+            }
+        }
+
+        return strs[0]; // Whole first string is the prefix
+    }
+
+    public static void main(String[] args) {
+        String[] input = {"flower", "flow", "flight"};
+        System.out.println("Longest Common Prefix: " + longestCommonPrefix(input));
+    }
+}
